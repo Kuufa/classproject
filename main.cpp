a22510de040a416d121b36e1fbd895799322796c
xZen-github <50919526+xZen-github@users.noreply.github.com>
2019-12-11 18:23:42 +0100

    updated
    added else to prevent putting random letters and ending program

--- main.cpp
+++ main.cpp
@@ -28,6 +28,7 @@ float divide (float x, float y)
 }
 
 
+
 int main()
 {
     int losowa = std::rand();
@@ -39,8 +40,20 @@ int main()
     std::cin >> choice;
         if (choice == '1') // kalkulator
         {
+            ujemna:
             std::cout << "Podaj 2 liczby \n";
-            std::cin >> x >> y;
+            std::cin >> x;
+                if (x<=0)
+                {
+                    std::cout << "Liczba musi byc dodatnia \n";
+                    goto ujemna;
+                }
+            std::cin >> y;
+                if (y<=0)
+                {
+                    std::cout << "Liczba musi byc dodatnia \n";
+                    goto ujemna;
+                }
             std::cout << "1) Dodawanie\n2) Odejmowanie\n3) Mnozenie\n4) Dzielenie \n";
             std::cin >> op;
                 if (op == '1') // dodawanie
@@ -108,6 +121,7 @@ int main()
                     }
                    }
 
+
         }
         else if (choice == '2') // obliczanie pola figur
         {
@@ -117,9 +131,9 @@ int main()
             std::cin >> op;
                 if (op == '1') // kwadrat
                 {
-                    std::cout << "Podaj dlugosci bokow \n";
-                    std::cin >> x >> y;
-                    std::cout << "Wynik:" << multiply(x, y) << "\n";
+                    std::cout << "Podaj dlugosc boku \n";
+                    std::cin >> x;
+                    std::cout << "Wynik:" << multiply(x, x) << "\n";
                     std::cout << "Czy chcesz obliczyc cos jeszcze? y/n \n";
                     std::cin >> exit;
                     if (exit == 'y')
@@ -189,6 +203,7 @@ int main()
                         goto orka;
                     }
                 }
+
         }
                 else if (choice == '3') // losowanie liczb
                 {
@@ -209,7 +224,24 @@ int main()
                     system("cls");
                     goto orka;
                 }
-        }
+            }
+               else
+                {
+                std::cout << "Co ty robisz \n";
+                std::cout << "Chcesz powrocic y/n \n";
+                std::cin >> exit;
+                if (exit == 'y')
+                {
+                    system("cls");
+                    goto utopia;
+                }
+                else if (exit == 'n')
+                {
+                    system("cls");
+                    goto orka;
+                }
+                }
+
 
     orka:
     return 0;
